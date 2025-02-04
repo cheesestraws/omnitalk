089cd0586c85e59c248b6b88fe3c3512fa40ebf4
cheesestraws <rob@midworld.co.uk>
2025-02-04 23:14:31 +0000

    mdns

---
+++ omnitalk/main/net/mdns.c
@@ -0,0 +1,25 @@
+#include "net/mdns.h"
+
+#include <esp_mac.h>
+#include <esp_err.h>
+#include <mdns.h>
+
+#include "net/common.h"
+
+void start_mdns(void) {
+	char *hostname = generate_hostname();
+	
+    ESP_ERROR_CHECK(mdns_init());
+    ESP_ERROR_CHECK(mdns_hostname_set(hostname));
+    ESP_ERROR_CHECK(mdns_instance_name_set(hostname));
+    
+    ESP_ERROR_CHECK(mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0));
+    
+    mdns_txt_item_t serviceTxtData[1] = {
+        {"path", "/metrics"},
+    };
+    
+	ESP_ERROR_CHECK(mdns_service_add(NULL, "_prometheus", "_tcp", 80, serviceTxtData, 1));
+	
+	free(hostname);
+}
--- omnitalk/main/net/net.c
+++ omnitalk/main/net/net.c
@@ -1,7 +1,9 @@
 #include "net/net.h"
 
 #include "net/ethernet.h"
+#include "net/mdns.h"
 
 void start_net(void) {
 	start_ethernet();
+	start_mdns();
 }
