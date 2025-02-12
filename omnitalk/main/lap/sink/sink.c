61320bb746acfc4bcc5540b1d80aded77d543c1e
cheesestraws <rob@midworld.co.uk>
2025-02-12 16:39:26 +0000

    give sink a bigger stack so it doesn't break

--- omnitalk/main/lap/sink/sink.c
+++ omnitalk/main/lap/sink/sink.c
@@ -3,18 +3,21 @@
 #include <stdio.h>
 
 #include <esp_err.h>
+#include <esp_log.h>
 
 #include "mem/buffers.h"
+
+static const char* TAG="SINK";
 
 static void sink_rx_runloop(void* tp) {
 	buffer_t *buff = NULL;
 	transport_t *transport = (transport_t*)tp;
 	ESP_ERROR_CHECK(enable_transport(transport));
-	printf("started sink for %s\n", transport->kind);
+	ESP_LOGI(TAG, "started sink for %s", transport->kind);
 	while(1) {
 		xQueueReceive(transport->inbound, &buff, portMAX_DELAY);
 		if (buff != NULL) {
-			printf("sink %s: received frame of %zu bytes", transport->kind, buff->length);
+			ESP_LOGI(TAG, "sink %s: received frame of %zu bytes", transport->kind, buff->length);
 			freebuf(buff);
 		}
 	}
@@ -23,7 +26,7 @@ static void sink_rx_runloop(void* tp) {
 
 esp_err_t start_sink(char* name, transport_t* transport) {
 	TaskHandle_t task;
-	if (xTaskCreate(&sink_rx_runloop, name, 1024, (void*)transport, 5, &task)) {
+	if (xTaskCreate(&sink_rx_runloop, name, 2048, (void*)transport, 5, &task)) {
 		return ESP_OK;
 	}
 	
