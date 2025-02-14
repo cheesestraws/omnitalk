5d25f590837088b8a99d7d8aa0df84127c4ee9be
cheesestraws <rob@midworld.co.uk>
2025-02-14 17:22:20 +0000

    initial LAP contract dump

---
+++ omnitalk/main/lap/lap.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <esp_err.h>
+
+#include "net/transport.h"
+
+typedef struct lap_s lap_t;
+
+typedef esp_err_t(*lap_void_handler)(lap_t*);
+
+
+// LAPs take in l2 frames/protocols from a transport
+// and turn them into DDP frames.
+struct lap_s {
+	void* private_data;
+	void* stats;
+	transport_t* transport;
+	
+	lap_void_handler enable;
+	lap_void_handler disable;
+	lap_void_handler acquire_address;
+	
+	QueueHandle_t inbound;
+	QueueHandle_t outbound;
+};
