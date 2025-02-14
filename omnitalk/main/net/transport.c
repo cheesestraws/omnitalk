9f15614bd3d6aa2d030948274a46a15a3dc9b364
cheesestraws <rob@midworld.co.uk>
2025-02-14 18:55:42 +0000

    utility functions for transports

--- omnitalk/main/net/transport.c
+++ omnitalk/main/net/transport.c
@@ -2,10 +2,29 @@
 
 #include <esp_err.h>
 
+#include "mem/buffers.h"
+
 esp_err_t enable_transport(transport_t* transport) {
 	return transport->enable(transport);
 }
 
 esp_err_t disable_transport(transport_t* transport) {
 	return transport->disable(transport);
+}
+
+buffer_t* trecv(transport_t* transport) {
+	buffer_t *buff = NULL;
+	xQueueReceive(transport->inbound, &buff, portMAX_DELAY);
+	return buff;
+}
+
+bool tsend(transport_t* transport, buffer_t *buff) {
+	BaseType_t err = xQueueSendToBack(transport->outbound,
+		buff, 0);
+	
+	if (err != pdTRUE) {
+		return false;
+	} 
+	
+	return true;
 }
--- omnitalk/main/net/transport.h
+++ omnitalk/main/net/transport.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stdbool.h>
+
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
 #include <esp_err.h>
+
+#include "mem/buffers.h"
 
 // transport.{c,h} defines the interface for a transport; a transport
 // is something that can ship l2-ish frames out of the router.
@@ -24,3 +28,13 @@ struct transport_s {
 
 esp_err_t enable_transport(transport_t* transport);
 esp_err_t disable_transport(transport_t* transport);
+
+// trecv is a utility function to receive a frame from a transport,
+// blocking until a frame is available.
+buffer_t* trecv(transport_t* transport);
+
+// tsend is a utility function to send a frame through a transport,
+// never blocking at all.  It returns true if the frame was sent and
+// ownership of the buffer was transferred to the transport, false
+// otherwise
+bool tsend(transport_t* transport, buffer_t *buff);
