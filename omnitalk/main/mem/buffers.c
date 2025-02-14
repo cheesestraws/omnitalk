d7cbc5aa283b9d46127ceefcacb30f865c0f4b17
cheesestraws <rob@midworld.co.uk>
2025-02-14 18:12:15 +0000

    add pointer to ddp packet to buffer

--- omnitalk/main/mem/buffers.c
+++ omnitalk/main/mem/buffers.c
@@ -1,5 +1,6 @@
 #include "mem/buffers.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 buffer_t *newbuf(size_t capacity) {
@@ -28,4 +29,22 @@ buffer_t *wrapbuf(void* data, size_t length) {
 
 void resetbuf(buffer_t *buffer) {
 	buffer->length = 0;
+}
+
+bool buf_set_ddp_info(buffer_t *buffer, uint32_t ddp_offset, buffer_ddp_type_t ddp_type) {
+	if (buffer == NULL || buffer->data == NULL) {
+		return false;
+	}
+	
+	if (ddp_offset > buffer->length) {
+		return false;
+	}
+	
+	buffer->ddp_type = ddp_type;
+	buffer->ddp_length = buffer->length - ddp_offset;
+	buffer->ddp_capacity = buffer->capacity - ddp_offset;
+	buffer->ddp_data = buffer->data + ddp_offset;
+	buffer->ddp_ready = true;
+	
+	return true;
 }
--- omnitalk/main/mem/buffers.h
+++ omnitalk/main/mem/buffers.h
@@ -1,15 +1,33 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
+typedef enum {
+	BUF_SHORT_HEADER = 1,
+	BUF_LONG_HEADER = 2
+} buffer_ddp_type_t;
+
+// a buffer is a ... buffer which will hold a DDP packet and some
+// L2 framing around it.
 typedef struct buffer_s {
+	// details about the memory buffer as a whole
 	size_t length;
 	size_t capacity;
 	uint8_t* data;
+	
+	// the l3 packet
+	bool ddp_ready;
+	buffer_ddp_type_t ddp_type;
+	size_t ddp_length;
+	size_t ddp_capacity;
+	uint8_t* ddp_data;
 } buffer_t;
 
 buffer_t *newbuf(size_t length);
 void freebuf(buffer_t *buffer_t);
 buffer_t *wrapbuf(void* data, size_t length);
 void resetbuf(buffer_t *buffer);
+
+bool buf_set_ddp_info(buffer_t *buffer, uint32_t ddp_offset, buffer_ddp_type_t ddp_type);
