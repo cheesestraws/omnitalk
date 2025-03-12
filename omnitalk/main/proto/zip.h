e608e6848642945daf2d1772c8ffd1ae81d095cb
cheesestraws <rob@midworld.co.uk>
2025-03-12 21:18:54 +0000

    zip: test query creation and filling-in

--- omnitalk/main/proto/zip.h
+++ omnitalk/main/proto/zip.h
@@ -14,15 +14,24 @@ struct zip_query_packet_s {
 	uint16_t networks[];
 } __attribute__((packed));
 
+typedef struct zip_query_packet_s zip_query_packet_t;
+
 static inline void zip_qry_setup_packet(buffer_t* buff, uint8_t network_count) {
-	struct zip_query_packet_s *packet = (struct zip_query_packet_s*)(DDP_BODY(buff));
+	zip_query_packet_t *packet = (zip_query_packet_t*)(DDP_BODY(buff));
 	packet->always_one = 1;
 	packet->network_count = network_count;
-	buff->ddp_payload_length = sizeof(struct zip_query_packet_s) + (2 * network_count);
+	buff->ddp_payload_length = sizeof(zip_query_packet_t) + (2 * network_count);
+}
+
+static inline uint8_t zip_packet_get_network_count(buffer_t* buff) {
+	// both queries and responses have the network count in the same place
+	// so we just pretend it's a query packet for now
+	zip_query_packet_t *packet = (zip_query_packet_t*)(DDP_BODY(buff));
+	return packet->network_count;
 }
 
 static inline bool zip_qry_set_network(buffer_t* buff, int idx, uint16_t network) {
-	struct zip_query_packet_s *packet = (struct zip_query_packet_s*)(DDP_BODY(buff));
+	zip_query_packet_t *packet = (zip_query_packet_t*)(DDP_BODY(buff));
 
 	if (unlikely(idx >= packet->network_count)) {
 		return false;
@@ -30,4 +39,14 @@ static inline bool zip_qry_set_network(buffer_t* buff, int idx, uint16_t network
 	
 	packet->networks[idx] = htons(network);
 	return true;
+}
+
+static inline uint16_t zip_qry_get_network(buffer_t* buff, int idx) {
+	zip_query_packet_t *packet = (zip_query_packet_t*)(DDP_BODY(buff));
+
+	if (unlikely(idx >= packet->network_count)) {
+		return 0;
+	}
+	
+	return ntohs(packet->networks[idx]);
 }
--- omnitalk/main/proto/zip_test.c
+++ omnitalk/main/proto/zip_test.c
@@ -5,5 +5,22 @@
 #include "test.h"
 
 TEST_FUNCTION(test_zip_qry_creation) {
-	TEST_FAIL("no test here yet");
+	buffer_t *buff;
+	
+	buff = newbuf_ddp();
+	zip_qry_setup_packet(buff, 2);
+	TEST_ASSERT(zip_packet_get_network_count(buff) == 2);
+	
+	// We should be able to set up some network numbers
+	TEST_ASSERT(zip_qry_set_network(buff, 0, 8));
+	TEST_ASSERT(zip_qry_set_network(buff, 1, 16));
+	
+	// We can't set off the end of the packet
+	TEST_ASSERT(!zip_qry_set_network(buff, 2, 32));
+	
+	// We should be able to retrieve network numbers, too
+	TEST_ASSERT(zip_qry_get_network(buff, 0) == 8);
+	TEST_ASSERT(zip_qry_get_network(buff, 1) == 16);
+	
+	TEST_OK();
 }
